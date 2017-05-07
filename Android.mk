83dc4cbf5e2d1e3ff211bcec2e5562717a60ed32
Adarsh-MR <adarshmr1998@gmail.com>
2017-05-07 07:13:30 +0000

    msm8996: media: remove project-path
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-media))
-
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
@@ -9,6 +7,4 @@ include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 
 ifeq ($(BOARD_USES_ADRENO), true)
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
-endif
-
 endif
